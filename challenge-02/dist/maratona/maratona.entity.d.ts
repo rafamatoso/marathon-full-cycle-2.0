63b3ba1414f303dfc04fb3d0d7b0e553dbc2f56c
Rafael Matoso <rafael.gustavo.gomes@concrete.com.br>
2020-05-05 23:30:21 -0300

    Some corrections in file structure

--- challenge-02/dist/maratona/maratona.entity.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Maratona {
-    private id;
-    private aula;
-    constructor(aula: string);
-    getId(): string;
-    setId(id: string): void;
-    getAula(): string;
-}
--- challenge-02/dist/maratona/maratona.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { Maratona } from './maratona.entity';
-export declare class MaratonaService {
-    private readonly maratonaRepository;
-    constructor(maratonaRepository: Repository<Maratona>);
-    findAll(): Promise<Maratona[]>;
-    findById(id: string): Promise<Maratona>;
-    create(maratona: Maratona): Promise<void>;
-}
